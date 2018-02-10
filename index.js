f24ecfe936381a9345954a607c4c60fd395e0465
Alan C <alanc@negativepop.com>
2018-02-09 19:59:12 -0500

    Added Dependencies and setup MongoDB

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
 //Express
 var express = require('express');
 var app = express();
+
 //MongoDB
-var mongo = require('mongodb');
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert')
+const url = 'mongodb://localhost:27017';
+
+MongoClient.connect(url, function(err, client) {
+    assert.equal(null, err);
+    console.log("Connected successfully to server");
+client.close();
+});
+
+//Mongoose
+var mongoose = require('mongoose');
+var mongoDB = 'mongodb://localhost/TEST';
+mongoose.connect(mongoDB)
+// Get Mongoose to use the global promise library
+mongoose.Promise = global.Promise;
+//Get the default connection
+var db = mongoose.connection;
 
 app.use('/', express.static('public/html'));
 app.use('/', express.static('public/css'));
 app.use('/', express.static('public/images'));
 app.use('/', express.static('public/js'));
+app.use('/', express.static('db'));
 
-app.get('/',(req,res) => res.sendFile('/index.html'))
+app.get('/',(req,res) => res.sendFile('/index.html'));
 
-app.listen(3000, () => console.log('This is working!'))
-
+app.listen(3000, () => console.log('This is working!'));
